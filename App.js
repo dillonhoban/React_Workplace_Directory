a5b20a8022881268739add0d82a2ea671b398f0d
Dillon H <dhobs21@gmail.com>
2021-02-25 22:22:06 -0800

    Adding base file strcture

---
+++ App.js
@@ -0,0 +1,7 @@
+import React, { useMemo, useState, useEffect } from "react";
+import ReactTable from "react-table-6";  
+import "react-table-6/react-table.css" 
+import Table from "./components/tablecomponent/table";
+import axios from "axios";
+import 'react-table-6/react-table.css';
+import Bootstrap from "bootstrap/dist/css/bootstrap.min.css";
